4b8a8cd5be3da207c49555a90fd1e7fc3968dd06
Nehaa Joshi <nehaajoshi2006@gmail.com>
2026-07-15 19:06:13 +0530

    Implement socket binding

--- src/server.cpp
+++ src/server.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
 Server::Server(int port)
@@ -14,10 +15,30 @@ Server::Server(int port)
 void Server::start()
 {
     std::cout << "Starting server... " << std::endl;
-    serverSocket=socket(AF_INET, SOCK_STREAM, 0);
+
+    serverSocket=socket(AF_INET, SOCK_STREAM, 0); //creating a TCP socket
+
     if (serverSocket <0){
         std::cerr << "Failed to create socket. :( " <<std::endl;
         return;
     }
+
     std::cout << "Socket created successfully. :D " <<std::endl;
+
+    sockaddr_in serverAddress{};
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port =htons(port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(serverSocket,reinterpret_cast<sockaddr*>(&serverAddress),sizeof(serverAddress))<0)
+    {
+        std::cerr << "Failed to bind socket. T^T " << std::endl;
+        close(serverSocket);
+        return;
+    }
+
+    std::cout << "Socket bound to port "
+              << port
+              << " successfully. " << std::endl;
 }
